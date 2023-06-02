86b632d0e4db16227a75f78e520751f385c83a1b
nakeking <1287414150@qq.com>
2023-06-02 14:23:35 +0800

    Dialogues补充，Header样式补充

--- src/application/Dialogues/index.tsx
+++ src/application/Dialogues/index.tsx
@@ -1,21 +1,30 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useId, useState } from 'react'
 
 import {
     MessageOutlined,
-    MoreOutlined
+    MoreOutlined,
+
+    EditFilled,
+    DeleteFilled
 } from '@ant-design/icons';
 import { SuiCorrugation } from '../../components';
 import { useTranslation } from 'react-i18next';
+import { Divider, Popover } from 'antd';
 
 type DialogueType = {
   id: number
   title: string
-}[]
+}
 
 const Dialogues: FC = () => {
   const {t} = useTranslation()
+  const [ popupContainer, setPopupContainer ] = useState<HTMLElement>()
 
-  const [ dialogues ] = useState<DialogueType>([
+  useEffect(() => {
+    setPopupContainer(document.getElementById('dialogues') as HTMLElement)
+  }, [popupContainer])
+
+  const [ dialogues ] = useState<DialogueType[]>([
     {id: 1, title: "Untitled"},
     {id: 2, title: "Untitled"},
     {id: 3, title: "Untitled"},
@@ -29,25 +38,62 @@ const Dialogues: FC = () => {
   ])
 
   return (
-      <div className='dialogues'>
+      <div id='dialogues' className='dialogues'>
           <div className='title'>{t("Dialogues.dialogues")}</div>
 
           <div className='dialogue'>
             { dialogues.map(dialogue => {
               return (
-                <SuiCorrugation key={dialogue.id}>
-                  <div className='item'>
-                    <MessageOutlined />
-                    <div className='title'>{dialogue.title}</div>
-                    <div className='more'>
-                      <MoreOutlined />
-                    </div>
-                  </div>
-                </SuiCorrugation>
+                <DialogueItem 
+                  key={dialogue.id} 
+                  dialogue={dialogue} 
+                  popupContainer={popupContainer as HTMLElement}/>
               )
             })}
           </div>
       </div>
+  )
+}
+
+interface DialogueItemProps {
+  dialogue: DialogueType,
+
+  popupContainer: HTMLElement
+}
+
+const DialogueItem: FC<DialogueItemProps> = (props) => {
+  const { dialogue, popupContainer } = props
+  const { t } = useTranslation()
+
+  return (
+    <SuiCorrugation>
+      <div className='dialogueItem'>
+        <MessageOutlined />
+        <div className='title'>{dialogue.title}</div>
+        <div className='more iconBase'>
+          <Popover
+            overlayClassName="dialoguePop"
+            placement="bottomRight"
+            arrow={false}
+            getPopupContainer={() => {
+              return popupContainer
+            }}
+            content={
+              <div className='popContent'>
+                <div className='popItem'>
+                  <EditFilled />{t("Dialogues.rename")}</div>
+                <span className='hr'></span>
+                <div className='popItem'>
+                  <DeleteFilled />{t("Dialogues.delete")}</div>
+              </div>
+            }
+            title={false}
+            trigger="click">
+            <MoreOutlined />
+          </Popover>
+        </div>
+      </div>
+    </SuiCorrugation>
   )
 }
 
--- src/application/Room/Header/index.tsx
+++ src/application/Room/Header/index.tsx
@@ -1,9 +1,23 @@
 import { FC } from "react";
 
+import {
+    MessageOutlined,
+
+    RestFilled,
+    SaveFilled
+} from '@ant-design/icons';
+
 const Header: FC = () => {
     return (
         <div className="header">
-
+            <div className="left">
+                <span className="iconBase"><MessageOutlined /></span>
+                <span className="title">Untitled</span>
+            </div>
+            <div className="right">
+                <span className="iconBase"><RestFilled /></span>
+                <span className="iconBase"><SaveFilled /></span>
+            </div>
         </div>
     )
 }
