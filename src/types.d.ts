79f9045c27c370fb3f2606feecba3573b2122d83
nakeking <1287414150@qq.com>
2023-06-06 00:56:54 +0800

    Markdown-it初始化

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,11 @@
+interface MessageType {
+    id: string,
+    role: "user" | "system",
+    content: string
+}
+
 export interface DialogueType {
     id: string
-    name: string
+    name: string,
+    messages?: MessageType[]
 }
